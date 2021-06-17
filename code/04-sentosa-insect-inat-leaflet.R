fa0f65bb87d811b5b945510875b9c35260cffb48
Tze Min <tzemin.koay@gmail.com>
2021-06-17 21:58:12 +0800

    added leaflet maps per species and per orders, with toggable control over: basemap theme, use of radio buttons vs check boxes, and clustering of data points

--- code/04-sentosa-insect-inat-leaflet.R
+++ code/04-sentosa-insect-inat-leaflet.R
@@ -29,7 +29,7 @@ initialise_script04 <- function() {
   return(full)
 }
 
-# ----------------------- Occurrences by Order -----------------------
+# ----------------------- Occurrences by Order: Trial -----------------------
 
 # Maps I want
 # - one map for all occurrences by order
@@ -46,6 +46,7 @@ full <- initialise_script04() %>%
                             "by", user_login))
 
 dfs_byorder <- full %>% split(list(.$order))
+dfs_byorder <- dfs_byorder[order(sapply(dfs_byorder, nrow), decreasing = TRUE)]
 
 basemap <- leaflet(full) %>%
   setView(lng = 103.8303, lat = 1.2494, zoom = 15) %>%
@@ -89,7 +90,19 @@ ordermap
 
 # ----------------------- Occurrences by Order: Dark Theme -----------------------
 
-makemap_order_dark <- function(insectdata, radio = FALSE) {
+library("leaflet")
+library("htmltools")
+library("htmlwidgets")
+
+full <- initialise_script04() %>% 
+  mutate(popup_text = paste("<i>", scientific_name_simple, "</i><br/>",
+                            observed_on, "<br/>",
+                            "by", user_login))
+
+dfs_byorder <- full %>% split(list(.$order))
+dfs_byorder <- dfs_byorder[order(sapply(dfs_byorder, nrow), decreasing = TRUE)]
+
+makemap_order <- function(insectdata, radio = FALSE) {
   
   basemap <- leaflet(insectdata) %>%
     setView(lng = 103.8303, lat = 1.2494, zoom = 15) %>%
@@ -142,4 +155,265 @@ makemap_order_dark <- function(insectdata, radio = FALSE) {
   ordermap
 }
 
-makemap_order_dark(full, radio = TRUE)
+# ----------------------- Occurrences by Order: Toggle Order Radio, Cluster and Basemap Theme at R Code -----------------------
+
+full <- initialise_script04() %>% 
+  mutate(popup_text = paste("<i>", scientific_name_simple, "</i><br/>",
+                            observed_on, "<br/>",
+                            "by", user_login))
+
+dfs_byorder <- full %>% split(list(.$order))
+dfs_byorder <- dfs_byorder[order(sapply(dfs_byorder, nrow), decreasing = TRUE)]
+
+makemap_order <- function(data, order_radio = TRUE, cluster = FALSE, theme = "Satellite") {
+  # available themes: Voyager, Dark, Light, Street, Satellite
+  
+  basemap <- leaflet(data) %>% 
+    setView(lng = 103.8303, lat = 1.2494, zoom = 15) %>%
+    purrr::when(theme == "Voyager" ~ addProviderTiles(., providers$CartoDB.Voyager),
+                theme == "Dark" ~ addProviderTiles(., providers$CartoDB.DarkMatter),
+                theme == "Light" ~ addProviderTiles(., providers$CartoDB.Positron),
+                theme == "Street" ~ addProviderTiles(., providers$OpenStreetMap),
+                theme == "Satellite" ~ addProviderTiles(., providers$Esri.WorldImagery))
+
+  num_of_orders <- length(dfs_byorder)
+  names_of_dfs <- names(dfs_byorder)
+  ordermap <- basemap
+  
+  if (order_radio == TRUE) {
+    # all data points will be red
+    
+    if (cluster == TRUE) {
+      for (i in seq(1:num_of_orders)) {
+        df <- dfs_byorder[i][[1]]
+        ordermap <- ordermap %>% 
+          addCircleMarkers(data = df,
+                           ~longitude, ~latitude,
+                           label = lapply(df$popup_text, htmltools::HTML),
+                           radius = 1.2, stroke = TRUE, opacity = 1, fillOpacity = 1,
+                           color = "red",
+                           group = names_of_dfs[i],
+                           clusterOptions = markerClusterOptions()) # the only difference between this and else cluster option
+      }
+      
+    } else {
+      for (i in seq(1:num_of_orders)) {
+        df <- dfs_byorder[i][[1]]
+        ordermap <- ordermap %>% 
+          addCircleMarkers(data = df,
+                           ~longitude, ~latitude,
+                           label = lapply(df$popup_text, htmltools::HTML),
+                           radius = 1.2, stroke = TRUE, opacity = 1, fillOpacity = 1,
+                           color = "red",
+                           group = names_of_dfs[i])
+      }
+    }
+    
+    ordermap <- ordermap %>%
+      addLayersControl(
+        baseGroups = names_of_dfs,
+        options = layersControlOptions(collapsed = FALSE))
+    
+  } else {
+    # order_radio == FALSE: all data points will have different colors across insect orders
+    
+    colors_per_df <- RColorBrewer::brewer.pal(num_of_orders, "Set1") # use a qualitative color palette
+    
+    if (cluster == TRUE) {
+      for (i in seq(1:num_of_orders)) {
+        df <- dfs_byorder[i][[1]]
+        ordermap <- ordermap %>% 
+          addCircleMarkers(data = df,
+                           ~longitude, ~latitude,
+                           label = lapply(df$popup_text, htmltools::HTML),
+                           radius = 1, stroke = TRUE, opacity = 1, fillOpacity = 1,
+                           color = colors_per_df[i],
+                           group = names_of_dfs[i],
+                           clusterOptions = markerClusterOptions()) # the only difference between this and else cluster option)
+      }
+      
+    } else {
+      for (i in seq(1:num_of_orders)) {
+        df <- dfs_byorder[i][[1]]
+        ordermap <- ordermap %>% 
+          addCircleMarkers(data = df,
+                           ~longitude, ~latitude,
+                           label = lapply(df$popup_text, htmltools::HTML),
+                           radius = 1, stroke = TRUE, opacity = 1, fillOpacity = 1,
+                           color = colors_per_df[i],
+                           group = names_of_dfs[i])
+      }
+    }
+    ordermap <- ordermap %>%
+      addLayersControl(
+        overlayGroups = names_of_dfs,
+        options = layersControlOptions(collapsed = FALSE)) %>%
+      addLegend("bottomright", 
+                colors = colors_per_df,
+                labels = names_of_dfs) %>%
+      hideGroup(names_of_dfs[2:num_of_orders])
+  }
+  ordermap
+}
+
+makemap_order(data = full, order_radio = TRUE, cluster = TRUE, theme = "Street")
+
+
+# ----------------------- Occurrences by Order: Same as above, except - Checkbox the Basemap Themes -----------------------
+
+full <- initialise_script04() %>% 
+  mutate(popup_text = paste("<i>", scientific_name_simple, "</i><br/>",
+                            observed_on, "<br/>",
+                            "by", user_login))
+
+dfs_byorder <- full %>% split(list(.$order))
+dfs_byorder <- dfs_byorder[order(sapply(dfs_byorder, nrow), decreasing = TRUE)]
+
+makemap_order <- function(data, order_radio = TRUE, cluster = FALSE) {
+
+  basemap <- leaflet(data) %>% 
+    setView(lng = 103.8303, lat = 1.2494, zoom = 15) %>%
+    addTiles(group = "Street") %>%
+    addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
+    addProviderTiles(providers$CartoDB.DarkMatter, group = "Dark") %>%
+    addProviderTiles(providers$CartoDB.Positron, group = "Light") %>%
+    addProviderTiles(providers$CartoDB.Voyager, group = "Voyager")
+  
+  num_of_orders <- length(dfs_byorder)
+  names_of_dfs <- names(dfs_byorder)
+  ordermap <- basemap
+  
+  if (order_radio == TRUE) {
+    # all data points will be red
+    
+    if (cluster == TRUE) {
+      for (i in seq(1:num_of_orders)) {
+        df <- dfs_byorder[i][[1]]
+        ordermap <- ordermap %>% 
+          addCircleMarkers(data = df,
+                           ~longitude, ~latitude,
+                           label = lapply(df$popup_text, htmltools::HTML),
+                           radius = 1.2, stroke = TRUE, opacity = 1, fillOpacity = 1,
+                           color = "red",
+                           group = names_of_dfs[i],
+                           clusterOptions = markerClusterOptions()) # the only difference between this and else cluster option
+      }
+      
+    } else {
+      for (i in seq(1:num_of_orders)) {
+        df <- dfs_byorder[i][[1]]
+        ordermap <- ordermap %>% 
+          addCircleMarkers(data = df,
+                           ~longitude, ~latitude,
+                           label = lapply(df$popup_text, htmltools::HTML),
+                           radius = 1.2, stroke = TRUE, opacity = 1, fillOpacity = 1,
+                           color = "red",
+                           group = names_of_dfs[i])
+      }
+    }
+    
+    ordermap <- ordermap %>%
+      addLayersControl(
+        baseGroups = names_of_dfs,
+        options = layersControlOptions(collapsed = FALSE))
+    
+  } else {
+    # order_radio == FALSE: all data points will have different colors across insect orders
+    
+    colors_per_df <- RColorBrewer::brewer.pal(num_of_orders, "Set1") # use a qualitative color palette
+    
+    if (cluster == TRUE) {
+      for (i in seq(1:num_of_orders)) {
+        df <- dfs_byorder[i][[1]]
+        ordermap <- ordermap %>% 
+          addCircleMarkers(data = df,
+                           ~longitude, ~latitude,
+                           label = lapply(df$popup_text, htmltools::HTML),
+                           radius = 1, stroke = TRUE, opacity = 1, fillOpacity = 1,
+                           color = colors_per_df[i],
+                           group = names_of_dfs[i],
+                           clusterOptions = markerClusterOptions()) # the only difference between this and else cluster option)
+      }
+      
+    } else {
+      # cluster == FALSE
+      for (i in seq(1:num_of_orders)) {
+        df <- dfs_byorder[i][[1]]
+        ordermap <- ordermap %>% 
+          addCircleMarkers(data = df,
+                           ~longitude, ~latitude,
+                           label = lapply(df$popup_text, htmltools::HTML),
+                           radius = 1, stroke = TRUE, opacity = 1, fillOpacity = 1,
+                           color = colors_per_df[i],
+                           group = names_of_dfs[i])
+      }
+    }
+    ordermap <- ordermap %>%
+      addLayersControl(
+        baseGroups = c("Voyager", "Street", "Satellite", "Light", "Dark"),
+        overlayGroups = names_of_dfs,
+        options = layersControlOptions(collapsed = FALSE)) %>%
+      addLegend("bottomright", 
+                colors = colors_per_df,
+                labels = names_of_dfs) %>%
+      hideGroup(names_of_dfs[2:num_of_orders])
+  }
+  ordermap
+}
+
+makemap_order(data = full, order_radio = TRUE, cluster = FALSE)
+
+
+# ----------------------- Map per Species -----------------------
+
+full <- initialise_script04() %>% 
+  mutate(popup_text = paste("<i>", scientific_name_simple, "</i><br/>",
+                            observed_on, "<br/>",
+                            "by", user_login))
+
+makemap_species <- function(data, speciesname, cluster = FALSE) {
+  # species must be only genus + species, no subspecies taken into account
+  
+  df <- data %>% filter(scientific_name_simple == speciesname)
+  count <- nrow(df)
+
+  maptitle <- tagList(
+    tags$h1(tags$b(speciesname), tags$style("h1 {display: inline; color: black; font-size: 15px; font-style: italic; text-align: center;}")),
+    tags$p(tags$b(paste0("(", count, ")")), tags$style("p {display: inline; color: black; font-size: 14px; text-align: center;}")))
+
+  map <- leaflet(df) %>% 
+    setView(lng = 103.8303, lat = 1.2494, zoom = 15) %>%
+    addTiles(group = "Street") %>%
+    addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
+    addProviderTiles(providers$CartoDB.DarkMatter, group = "Dark") %>%
+    addProviderTiles(providers$CartoDB.Positron, group = "Light") %>%
+    addProviderTiles(providers$CartoDB.Voyager, group = "Voyager")
+  
+  if (cluster == TRUE) {
+    map <- map %>%
+      addCircleMarkers(data = df,
+                       ~longitude, ~latitude,
+                       label = lapply(df$popup_text, htmltools::HTML),
+                       radius = 1.2, stroke = TRUE, opacity = 1, fillOpacity = 1,
+                       color = "red",
+                       clusterOptions = markerClusterOptions()) # the only difference between this and else cluster option
+
+    } else {
+      map <- map %>%
+        addCircleMarkers(data = df,
+                         ~longitude, ~latitude,
+                         label = lapply(df$popup_text, htmltools::HTML),
+                         radius = 1.2, stroke = TRUE, opacity = 1, fillOpacity = 1,
+                         color = "red")
+
+    }
+
+     map %>%
+       addControl(maptitle, position = "bottomleft") %>%
+       addLayersControl(
+         baseGroups = c("Voyager", "Street", "Satellite", "Light", "Dark"),
+         options = layersControlOptions(collapsed = FALSE))
+  
+}
+
+makemap_species(data = full, speciesname = "Elymnias hypermnestra", cluster = FALSE)
